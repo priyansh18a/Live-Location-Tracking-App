8691d267bd063c487f3adf66aa167da0e76dc1a8
soumya gupta <samygutu.9995@gmail.com>
2021-07-12 05:55:38 +0530

    authentication done

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:authentification/Login.dart';
+import 'package:authentification/SignUp.dart';
+import 'package:authentification/Start.dart';
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+   runApp(MyApp());
+   }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(
+        primaryColor: Colors.orange
+      ),
+      debugShowCheckedModeBanner: false,
+      home: 
+    
+      HomePage(),
+
+      routes: <String,WidgetBuilder>{
+
+        "Login" : (BuildContext context)=>Login(),
+        "SignUp":(BuildContext context)=>SignUp(),
+        "start":(BuildContext context)=>Start(),
+      },
+      
+    );
+  }
+
+}
+
+
+
