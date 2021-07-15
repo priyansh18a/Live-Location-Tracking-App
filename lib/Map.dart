1def2f4528271ba21e13044d733e196ce853f322
Priyanshu Gupta <priyansh18a@iitg.ac.in>
2021-07-15 18:38:41 +0530

    'task-3

--- lib/Map.dart
+++ lib/Map.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +5,7 @@ import 'package:location/location.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
-
+import 'package:rxdart/rxdart.dart';
 
 
 class Map extends StatefulWidget {
@@ -20,9 +19,13 @@ class _MapState extends State<Map> {
   Completer<GoogleMapController> mapController = Completer();
   Location location = new Location();
   final geo = Geoflutterfire();
+  BehaviorSubject<double> radius = BehaviorSubject();
+  Stream<dynamic> query;
+  StreamSubscription subscription;
+  Set<Marker> customMarkers = {};
 
-
-
+  double lat ;
+  double long;
 
   checkAuthentification() async {
     _auth.authStateChanges().listen((user) {
@@ -32,27 +35,78 @@ class _MapState extends State<Map> {
     });
   }
 
-  void _onMapCreated(GoogleMapController controller) {
+  void _onMapCreated(GoogleMapController controller)
+  {
+    // location.onLocationChanged.listen((position) {
+    //   GeoFirePoint myLocation = geo.point(
+    //       latitude: position.latitude, longitude: pos.longitude);
+
+      // firestore.collection('locations')
+      //     .add({'name': 'myLocation', 'position': myLocation.data});
+    // });
     mapController.complete(controller);
   }
 
-  userLocation() async {
-    var pos = await location.getLocation();
-    GeoFirePoint myLocation = geo.point(latitude: pos.latitude, longitude:  pos.longitude);
-    firestore.collection('locations')
-        .add({'name': 'myLocation', 'position': myLocation.data});
+  void updateMarkers(List<DocumentSnapshot> documentList){
+    customMarkers = {};
+    documentList.forEach((DocumentSnapshot document) {
+      GeoPoint pos = document['position']['geopoint'];
+      Marker marker = Marker(
+        markerId: MarkerId(document['name']),
+        position: LatLng(pos.latitude, pos.longitude),
+        infoWindow: InfoWindow(title: document['name'], snippet: document['name']),
+        icon: BitmapDescriptor.defaultMarker,
+      );
+      setState(() {
+        customMarkers.add(marker);
+      });
+    });
   }
 
+  setQuery() async {
+    var pos = await location.getLocation();
+    this.lat = pos.latitude;
+    this.long = pos.longitude;
+    GeoFirePoint myLocation = geo.point(latitude: lat, longitude: long);
+
+    var ref = firestore.collection('locations');
+
+    final GoogleMapController controller = await mapController.future;
+    controller.animateCamera(CameraUpdate.newLatLng(new LatLng(lat, long)));
+
+    subscription = radius.switchMap((rad) {
+      return geo.collection(collectionRef: ref).within(
+          center: myLocation,
+          radius: rad,
+          field: 'position',
+          strictMode: true
+      );
+    }).listen(updateMarkers);
+
+  }
+
+  _updateQuery(value) async {
+    // var pos = await location.getLocation();
+    setState(() {
+      radius.add(value);
+    });
+    final zoomMap = {100.0: 12.0, 150.0: 11.0, 200: 10.0, 250.0: 9.0,300:8.0, 350.0: 7.0, 400.0: 6.0, 450: 5.0, 500.0: 4.0};
+    final num zoom = zoomMap[value];
+    final GoogleMapController controller = await mapController.future;
+    controller.moveCamera(CameraUpdate.zoomTo(zoom));
+    // controller.moveCamera(CameraUpdate.newLatLngZoom(new LatLng(lat, long), zoom));
+  }
 
   @override
   void initState() {
     super.initState();
     this.checkAuthentification();
-    this.userLocation();
+    radius.add(100.0);
+    setQuery();
   }
 
   @override
-  build(context) {
+  Widget build(BuildContext context)  {
     return Scaffold(
             appBar: AppBar(
               title: const Text('Google Map'),
@@ -100,15 +154,28 @@ class _MapState extends State<Map> {
                   GoogleMap(
                     mapType: MapType.normal,
                     initialCameraPosition: CameraPosition(
-                      target: LatLng(37.42796133580664, -122.085749655962),
+                      target: LatLng(26.8467088, 80.9461592),
                       zoom: 14.4746,
                     ),
-                    onMapCreated: _onMapCreated ,
-                    myLocationEnabled: true,
-                  )]
-                ),
+                    onMapCreated: _onMapCreated,
+                    markers: customMarkers,
+                  ),
+                  Positioned(
+                      bottom: 50,
+                      left: 10,
+                      child: Slider(
+                        min: 100.0,
+                        max: 500.0,
+                        divisions: 8,
+                        value: radius.value,
+                        label: 'Radius ${radius.value} kms',
+                        activeColor: Colors.blue,
+                        inactiveColor: Colors.blue.withOpacity(0.3),
+                        onChanged: _updateQuery,
+                      )
+                  )
+                ]),
                 floatingActionButton: FloatingActionButton.extended(
-                  onPressed: _goToTheLake,
                   label: Text('To the lake!'),
                   icon: Icon(Icons.location_on),
                 ),
@@ -116,14 +183,12 @@ class _MapState extends State<Map> {
   }
 
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await mapController.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(
-            bearing: 192.8334901395799,
-            target: LatLng(37.43296265331, -122.08832357078792),
-            tilt: 59.440717697143555,
-            zoom: 19.151926040649414)));
+
+  @override
+  void dispose() {
+    radius.close();
+    subscription.cancel();
+    super.dispose();
   }
 
 }
